85a63cce9b6768e2ba24f942006c1ea129991615
Chalysh Dmitrii <faimos69@gmail.com>
2025-09-01 21:44:41 +0600

    update PWA configs

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.ico'],
       manifest: {
         name: 'Учет ремонтов',
         short_name: 'Ремонты',
